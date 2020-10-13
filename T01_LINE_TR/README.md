e84073a67c30fd7807cc5de587187eafbfe367da
HALKA9000-NIIP <csta19097@g.nihon-u.ac.jp>
2020-10-13 22:38:30 +0900

    new init to head

---
+++ T01_LINE_TR/README.md
@@ -0,0 +1,2 @@
+# S-LPESSU: Standard LineTR Project for Experience in School Students Use-case
+txrファイルは出力データのために作成した形式 。プログラム(app.c)中では
---
+++ T01_LINE_TR/app.h
@@ -0,0 +1,11 @@
+#include "target_test.h"
+
+#ifndef STACK_SIZE
+#define STACK_SIZE 4096
+#endif /* STACK_SIZE */
+
+/* 関数のプロトタイプ宣言 */
+#ifndef TOPPERS_MACRO_ONLY
+extern void main_task(intptr_t exinf);
+extern void run_task(intptr_t exinf);
+#endif /* TOPPERS_MACRO_ONLY */
